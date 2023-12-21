b73db625b42b0471a21b20a665a986d5bfa03e13
hpower2 <irvineafri@gmail.com>
2023-12-21 17:07:27 +0700

    fixing sedikit di discovery

--- config.sh
+++ config.sh
@@ -4,6 +4,6 @@ export discovery_name=Discovery-Service-0.0.1.jar
 export discovery_clone=1
 export discovery_default_port=8761
 export discovery_file=target/$discovery_name
-export discovery_service=discover_service
+export discovery_service=discovery-service
 export discovery_build=build/
 export discovery_build_file=build/$discovery_name
--- gen-disc.sh
+++ gen-disc.sh
@@ -4,6 +4,6 @@ source ./config.sh
 args=()
 for ((i=$discovery_default_port; i<$discovery_default_port+$discovery_clone; i++)); do
    export current_port=$i
-   pm2 start "./run-service.sh" --name "$discovery_service-$i" --namespace "$discovery_service"
+   pm2 start "./run-service.sh" --name "$discovery_service-$i" --namespace "$discovery_service" --no-autorestart
 done
 
